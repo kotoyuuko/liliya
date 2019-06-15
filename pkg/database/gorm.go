dd396fb1630bc9c6d966ac768fff0c763c8e39a0
kotoyuuko <kokoro@core.moe>
2019-06-15 17:11:44 +0800

    use a function to generate connection string

--- pkg/database/gorm.go
+++ pkg/database/gorm.go
@@ -1,6 +1,10 @@
 package database
 
-import "github.com/jinzhu/gorm"
+import (
+	"net/url"
+
+	"github.com/jinzhu/gorm"
+)
 
 // Connect returns a database instance
 func Connect(dialect, args string) (*gorm.DB, error) {
@@ -10,4 +14,12 @@ func Connect(dialect, args string) (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+// ArgsString returns a string contains connection information
+func ArgsString(host, user, password, name, timezone string) string {
+	args := user + ":" + password + "@tcp(" + host + ")/"
+	args = args + name + "?charset=utf8&parseTime=True&loc=" + url.QueryEscape(timezone)
+
+	return args
 }
